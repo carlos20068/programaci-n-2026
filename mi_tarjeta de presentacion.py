8b481954a75893ef14a88e40a5d9456541fcdbc1
carlos20068 <crmelgars@alumno.uspg.edu.gt>
2026-02-18 13:03:07 -0600

    Add files via upload
    con sus códigos

---
+++ mi_tarjeta de presentacion.py
@@ -0,0 +1,13 @@
+#programa para crear una tarjeta de presentacion
+#pedir datos del usuario
+input("ingresa su nombre: ")
+input ("ingresa tu edad:")
+input ("ingresa tu ciudad:")
+input ("ingresa tu hobby:")
+# Mostrar la tarjeta de presentacion
+print("\n=====Tarjeta de Presentación=====")
+print("Nombre: ", input("ingresa su nombre: "))
+print("Edad: ", input ("ingresa tu edad:"))
+print("Ciudad: ", input ("ingresa tu ciudad:"))
+print("Hobby: ", input ("ingresa tu hobby:"))
+print("=================================#")
